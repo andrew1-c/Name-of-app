1db4617ef91f9b1f7e6ba6786e5ba1b23851fe72
Andrew Correll <andrew.correll.dev@gmail.com>
2017-07-07 05:35:11 -0500

    should fix all link issue bugs updated routes.rb and changed links to them

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :products
   resources :object_books
-  get 'static_pages/about'
+  get 'static_pages/about', to: "static_pages#about", as: "about"
 
-  get 'static_pages/contact'
+  get 'static_pages/contact', to: "static_pages#contact", as: "contact"
 
-  get 'static_pages/index'
+  get 'static_pages/index', as: "home"
   root 'static_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
