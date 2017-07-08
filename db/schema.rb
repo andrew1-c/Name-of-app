ee9279949056db75f0934524f9d1eb220a2395c6
Andrew Correll <andrew.correll.dev@gmail.com>
2017-07-08 04:48:16 -0500

    4.2 added colorfield/pics/product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707071611) do
+ActiveRecord::Schema.define(version: 20170708085547) do
 
   create_table "object_books", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170707071611) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "colour"
   end
 
 end
