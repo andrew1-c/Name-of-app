4e45fee68f3415101afea70d01f1cada4a2dd253
Andrew Correll <andrew.correll.dev@gmail.com>
2017-07-08 12:38:20 -0500

    4.3 added more routes/productlandingpage/orders expanded nav

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+ 
 
   # GET /products
   # GET /products.json
@@ -16,6 +17,7 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
 
   # GET /products/1/edit
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   get 'static_pages/contact', to: "static_pages#contact", as: "contact"
 
   get 'static_pages/index', as: "home"
-  root 'static_pages#index'
+  get 'products', to: "products#index", as:"productsp"
+  get 'orders/index', to: "orders#index", as: "ordersp"
 
+
+  root 'static_pages#landing_page'
+  resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
